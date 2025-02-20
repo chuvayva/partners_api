9f58c19c759809114f520e47c508d37f0f504bd8
Vitaliy Kireev <chuvayva@gmail.com>
2025-02-20 12:22:20 +0400

    Convert operating_area column to PostGIS geometry for better performance (#5)

--- app/utils/geo_helper.rb
+++ app/utils/geo_helper.rb
@@ -1,8 +1,11 @@
 module GeoHelper
   class << self
     def operating_area(address, operating_radius)
-      factory = RGeo::Geographic.spherical_factory(srid: 4326)
-      factory.point(address.location.x, address.location.y).buffer(operating_radius)
+      sql = ActiveRecord::Base.sanitize_sql([ <<-SQL.squish, address.location.x, address.location.y, operating_radius ])
+        SELECT ST_Buffer(ST_MakePoint(?, ?)::geography, ?);
+      SQL
+
+      ActiveRecord::Base.connection.select_value(sql)
     end
   end
 end
